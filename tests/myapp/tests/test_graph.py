46e20ed28871315dbba96bedab16e3b9572a0404
Mike Johnson <mike@publicstatic.net>
2011-08-19 23:03:24 -0700

    found a good toposort

--- tests/myapp/tests/test_graph.py
+++ tests/myapp/tests/test_graph.py
@@ -9,20 +9,18 @@ class TestGraph(TestCase):
         g[3] = 2
         self.assertEqual(g[3], 2)
 
-    def test_leafs(self):
+    def test_multiple(self):
         g = Digraph()
         g.arc(1, 2)
-        g.arc(2, 3)
-        g.arc(3, 2)
-        g.arc(4)
-
-        leafs = list(g.leafs())
-        self.assertEqual([4], leafs)
+        g.arc(1, 3)
+        self.assertEquals([2, 3], list(g[1]))
+        self.assertTrue(2 in g)
+        self.assertTrue(3 in g)
 
     def test_roots(self):
         g = Digraph([1, 2, 3])
         g.arc(3, 4) # arc should create a key for 4
-        self.assertEqual([1, 2, 4], list(g.leafs()))
+        self.assertEqual([1, 2, 3], list(g.roots()))
 
     def test_find_roots(self):
         g = Digraph([1, 2, 3])
@@ -37,6 +35,7 @@ class TestGraph(TestCase):
         g.arc(2, 1)
         self.assertEqual([], g.roots())
         g.arc(3)
+        self.assertTrue(3 in g)
         self.assertEqual([3], g.roots())
 
     def test_traverse(self):
@@ -46,14 +45,16 @@ class TestGraph(TestCase):
         g.arc(3, 6)
         g.arc(4)
 
+        self.assertEquals([1, 4], g.roots())
         self.assertEquals([1, 4, 2, 3, 6], list(g.traverse()))
 
     def test_dups(self):
         g = Digraph()
-        g.arc(1, 2)
-        g.arc(3, 2)
-        g.arc(4, 2)
-        self.assertEquals([1, 3, 4, 2], list(g.traverse()))
+        #g.arc(1, 2)
+        g.arc(3, 1, 2)
+        g.arc(4, 1, 2, 3)
+        self.assertEquals([4, 1, 2, 3], list(g.traverse()))
+
         g = Digraph()
         g.arc(1, 2)
         g.arc(3, 2)
@@ -66,5 +67,17 @@ class TestGraph(TestCase):
         g.arc(3, 2)
         g.arc(4, 2)
         g.arc(2, 6)
-        g.arc(15, 2) # root
+        g.arc(15, 2)
+        self.assertEquals([1, 4, 15], g.roots())
         self.assertEquals([1, 4, 15, 3, 2, 6], list(g.traverse()))
+
+        '''
+reload(graph)
+g = graph.Digraph()
+g.arc(1, 3)
+g.arc(3, 2)
+g.arc(4, 2)
+g.arc(2, 6)
+g.arc(15, 2)
+g.traverse()
+'''
